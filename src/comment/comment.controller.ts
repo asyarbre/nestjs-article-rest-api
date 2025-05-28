0eba16b8be887f6c5d920583966186452bbc84c7
Asya'ri <asyari818@gmail.com>
2025-05-28 08:50:37 +0700

    feat(comment): implement create and update for comments

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { AuthGuard } from '@/auth/guard/auth.guard';
+import { CommentService } from '@/comment/comment.service';
+import { CreateUpdateCommentDto } from '@/comment/dto/create-update-comment.dto';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 
+interface AuthenticatedRequest extends Request {
+  user: {
+    id: string;
+  };
+}
 @Controller('comment')
-export class CommentController {}
+@UseGuards(AuthGuard)
+export class CommentController {
+  constructor(private readonly commentService: CommentService) {}
+
+  @Post()
+  async createOrUpdateComment(
+    @Request() req: AuthenticatedRequest,
+    @Body() createUpdateCommentDto: CreateUpdateCommentDto,
+  ): Promise<{ message: string }> {
+    return await this.commentService.updateOrCreateComment(
+      req.user.id,
+      createUpdateCommentDto,
+    );
+  }
+}
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Article } from '@/article/entities/article.entity';
+import { CreateUpdateCommentDto } from '@/comment/dto/create-update-comment.dto';
+import { Comment } from '@/comment/entities/comment.entity';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(
+    @InjectRepository(Comment)
+    private CommentRepository: Repository<Comment>,
+
+    @InjectRepository(Article)
+    private ArticleRepository: Repository<Article>,
+  ) {}
+
+  async updateOrCreateComment(
+    userId: string,
+    createUpdateCommentDto: CreateUpdateCommentDto,
+  ): Promise<{ message: string }> {
+    const article = await this.ArticleRepository.findOne({
+      where: { id: createUpdateCommentDto.articleId },
+    });
+    if (!article) {
+      throw new NotFoundException(
+        `Article with id ${createUpdateCommentDto.articleId} not found`,
+      );
+    }
+
+    const comment = await this.CommentRepository.findOne({
+      where: {
+        articleId: createUpdateCommentDto.articleId,
+        userId: userId,
+      },
+    });
+    if (!comment) {
+      const newComment = this.CommentRepository.create({
+        ...createUpdateCommentDto,
+        userId: userId,
+      });
+      await this.CommentRepository.save(newComment);
+      return { message: 'Comment created successfully' };
+    } else {
+      Object.assign(comment, createUpdateCommentDto);
+      await this.CommentRepository.save(comment);
+      return { message: 'Comment updated successfully' };
+    }
+  }
+}
